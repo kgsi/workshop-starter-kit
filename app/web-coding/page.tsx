a609599deb1a2854a2e98a5f5734672ce9b3e025
Shinichi Kogiso <kuusai1998@gmail.com>
2025-08-30 20:54:54 +0900

    フィードバックセクションを`web-coding`ページに追加

--- app/web-coding/page.tsx
+++ app/web-coding/page.tsx
@@ -1,4 +1,5 @@
 import Site from "./Site";
+import FeedbackSection from "./FeedbackSection";
 
 export const dynamic = "force-static";
 
@@ -6,7 +7,7 @@ export default function WebCodingPage() {
   return (
     <div className="min-h-screen w-full">
       <Site />
+      <FeedbackSection />
     </div>
   );
 }
-
